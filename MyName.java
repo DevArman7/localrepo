d36e3be34eea01a70e30599a12a3816869e97ab6
DevArman7 <armanaman570@gmail.com>
2025-09-24 19:47:55 +0530

    Add Initial Files

---
+++ MyName.java
@@ -0,0 +1,10 @@
+package LocalRepo;
+
+import java.util.*;
+public class MyName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("My name is " + name);
+    }
+}
